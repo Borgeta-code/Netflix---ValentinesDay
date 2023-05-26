80141ddbe30315f28d9abb194936c919ae83e0cd
Borgesta-code <matheusborges123567gmail.com>
2023-05-26 12:30:30 -0300

    On production

--- content.js
+++ content.js
@@ -4,7 +4,7 @@ window.addEventListener("scroll", function () {
 
   if (scrollPosition > 50) {
     // Altere o valor aqui para determinar a posição em que a cor deve ser alterada
-    nav.style.backgroundColor = "rgba(17, 17, 17, 0.815)";
+    nav.style.backgroundColor = "rgba(17, 17, 17, 0.9)";
   } else {
     nav.style.backgroundColor = "transparent"; // Define a cor de fundo original da nav quando a posição de rolagem for menor que 100
   }
